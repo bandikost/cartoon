081b9739ee8a67c7b9c2ae69dd7a098cb9b03aeb
Vasiliy Pupkin <vpupkin@company.com>
2022-11-28 22:01:48 +0300

    first commit

---
+++ .history/src/components/userDetails_20221128175301.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export default class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: "",
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/userData", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        token: window.localStorage.getItem("token"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userData");
+        this.setState({ userData: data.data });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <p>Вы успешно вошли в учетную запись!</p>
+        Name<h1>{this.state.userData.fname}</h1>
+        Email <h1>{this.state.userData.email}</h1>
+        <button style={{ marginTop: 20, display: "flex", justifyContent: "center", alignItems: "center" }}><a href="/main.html"> Вернуться к мультфильмам</a></button>
+      </div>
+      
+    );
+  }
+}
