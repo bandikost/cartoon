59ac44da4b610e36e55d3cc81cadcbf8805da053
Vasiliy Pupkin <vpupkin@company.com>
2022-12-02 04:18:30 +0300

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 
+// Прелоадер
+
 let mask = document.querySelector('.mask');
 
 window.addEventListener('load', () => {
@@ -16,9 +18,31 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 
+// Облако в планетах
+
+
 $(document).ready(function() {
     $('.planet__burger').click(function(event) {
         $('.planet__burger, .planet__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+});
+
+
+
+// Кнопка Загрузить еще
+
+const showMore = document.querySelector('.show-more');
+const productsLength = document.querySelectorAll('.products-grid__item').length;
+let items = 3;
+showMore.addEventListener('click', () => {
+	items += 3;
+	const array = Array.from(document.querySelector('.products-grid').children);
+	const visItems = array.slice(0, items);
+
+	visItems.forEach(el => el.classList.add('is-visible'));
+
+	if (visItems.length === productsLength) {
+		showMore.style.display = 'none';
+	}
 });
